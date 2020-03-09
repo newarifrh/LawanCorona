66876f378cdf67cdc812f3f0c3ae12c967421289
Arif Rahman Hakim <16.9025@stis.ac.id>
2020-03-10 01:22:42 +0700

    remove unused dependencies

--- app/src/main/java/dev/blank/lawancorona/data/network/RetrofitClient.kt
+++ app/src/main/java/dev/blank/lawancorona/data/network/RetrofitClient.kt
@@ -3,7 +3,6 @@ package dev.blank.lawancorona.data.network
 import okhttp3.ConnectionSpec
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,14 +11,11 @@ object RetrofitClient {
     private const val url = "https://api.kawalcorona.com/"
     val service: Retrofit
         get() {
-            val interceptor = HttpLoggingInterceptor()
-            interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.BODY }
-            val httpClient = OkHttpClient.Builder().addInterceptor(interceptor)
-
+            val httpClient = OkHttpClient.Builder()
             httpClient.addInterceptor { chain: Interceptor.Chain ->
                 val original = chain.request()
                 val request = original.newBuilder()
-                        .method(original.method, original.body)
+                        .method(original.method(), original.body())
                         .build()
                 chain.proceed(request)
             }
